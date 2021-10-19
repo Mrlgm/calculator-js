cb391b1b0e20aa53a1728f930d2858214a828cac
mrlgm <mrlgm@foxmail.com>
2021-10-20 01:20:34 +0800

    feat: 支持多个数相加

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const addition = "12+34";
+const addition = "12+34+56";
 
 const isDigit = (char) => {
   return /[0-9]/.test(char);
@@ -34,12 +34,58 @@ function tokenizer(code) {
   return tokens;
 }
 
-function evaluate(tokens) {
+class AstNode {
+  nodeType = "";
+  children = [];
+  text = "";
+
+  constructor(nodeType, text) {
+    this.nodeType = nodeType;
+    text && (this.text = text);
+  }
+
+  addChild(node) {
+    this.children.push(node);
+  }
+}
+
+function parse(tokens) {
+  let token = tokens.shift();
+  let child1 = new AstNode(token.type, token.value);
+  let node = child1;
+  while (true) {
+    token = tokens.shift();
+    if (token && token.type === "symbol" && token.value === "+") {
+      node = new AstNode("add", token.value);
+      token = tokens.shift();
+      let child2 = new AstNode(token.type, token.value);
+      node.addChild(child1);
+      node.addChild(child2);
+      child1 = node;
+    } else {
+      break;
+    }
+  }
+  return node;
+}
+
+function evaluate(astNode) {
+  let child1, child2;
+  let value1, value2;
   let result;
-  if (tokens[1].type === "symbol" && tokens[1].value === "+") {
-    let value1 = Number(tokens[0].value);
-    let value2 = Number(tokens[2].value);
-    result = value1 + value2;
+
+  switch (astNode.nodeType) {
+    case "add":
+      child1 = astNode.children[0];
+      value1 = evaluate(child1);
+      child2 = astNode.children[1];
+      value2 = evaluate(child2);
+
+      result = value1 + value2;
+      break;
+    case "number":
+      result = Number(astNode.text);
+      break;
   }
 
   return result;
@@ -47,5 +93,7 @@ function evaluate(tokens) {
 
 const tokens = tokenizer(addition);
 console.log(tokens);
-const result = evaluate(tokens);
+const ast = parse(tokens)
+console.log(ast)
+const result = evaluate(ast);
 console.log(result);
